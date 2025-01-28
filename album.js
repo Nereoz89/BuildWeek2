2a0b5767fe5d26130501a79b11fdfa9019921dbf
Andrea Zimbardo <andreazimbardo89@gmail.com>
2025-01-28 15:58:05 +0100

    lista album ok

--- album.js
+++ album.js
@@ -1,8 +1,7 @@
-const addressBarContent = new URLSearchParams(location.search);
-const albumId = addressBarContent.get("id");
-console.log(albumId);
+const addressBarContent = new URLSearchParams(window.location.search);
+const albumId = addressBarContent.get("albumId");
 
-const url = `https://deezerdevs-deezer.p.rapidapi.com/album/${albumId}`;
+const url = "https://deezerdevs-deezer.p.rapidapi.com/album/";
 const options = {
   method: "GET",
   headers: {
@@ -11,17 +10,55 @@ const options = {
   },
 };
 
-fetch(url, options)
+fetch(`${url}${albumId}`, options)
   .then((res) => {
     if (res.ok) {
       return res.json();
     } else {
-      throw new Error("Errore");
+      throw new Error(`Errore: ${res.status} ${res.statusText}`);
     }
   })
   .then((data) => {
     console.log(data);
+    generateTracks(data);
   })
   .catch((err) => {
-    console.log(err);
+    console.error(err.message);
   });
+
+const mainRow = document.getElementById("albumContent");
+
+// FUNCTION PER GENERARE LE TRACKS
+const generateTracks = (tracks) => {
+  tracks.tracks.data.forEach((el) => {
+    let seconds = el.duration % 60;
+    seconds = seconds.toString().padStart(2, "0");
+    const options = {
+      style: "decimal",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+    };
+
+    const content = document.createElement("div");
+    content.classList.add("d-flex", "align-items-center");
+    content.innerHTML = `
+      <div class="col col-md-8 d-flex justify-content-between">
+         <div>
+           <h6 class="mb-0">${el.title}</h6>
+           <p class='text-secondary'>${el.artist.name}</p>
+         </div>
+         <div class="d-flex align-items-center">
+           <i
+             style="font-size: 1.5em"
+             class="bi bi-three-dots-vertical d-md-none"
+           ></i>
+           <p class="mb-0 d-none d-md-block">${el.rank.toLocaleString("it-IT", options)}</p>
+         </div>
+        </div>
+         <div class="col d-flex justify-content-end d-none d-md-flex">
+            <p class="mb-0 d-none d-md-block">${Math.floor(el.duration / 60)}:${seconds}</p>
+          </div>
+      `;
+    mainRow.appendChild(content);
+  });
+};
